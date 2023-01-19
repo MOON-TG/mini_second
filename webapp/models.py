fee506c53ee841fb4ace833e3ff32531fbe00cfc
MOON-TG <takeyou@kakao.com>
2023-01-19 09:54:13 +0900

    info admin 작성 가능 구현

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Info(models.Model):
+   battery = models.CharField(max_length=20)
+   color = models.CharField(max_length=20)
+
+   def __str__(self):
+      return f'[{self.pk}]{self.battery}{self.color}'
+
+   
