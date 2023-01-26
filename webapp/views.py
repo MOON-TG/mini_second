2cf371f7fa73d963e5950c29effefe3e1c600dd5
MOON-TG <takeyou@kakao.com>
2023-01-26 11:31:51 +0900

    아코디언 추가

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,5 @@
+from django.contrib import auth
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from .models import Info
 from django.views.generic import ListView
@@ -14,9 +16,34 @@ def createform(request):
    std.battery= request.GET['battery']
    std.color = request.GET['color']
    std.runtime = request.GET['runtime']
+   std.firmV = request.GET['firmV']
 
    std.save()
 
-   return redirect('http://127.0.0.1:8000#about')
+   return redirect('/#about')
+
+
+
+def signin(request):
+    if request.method == "GET":
+        return redirect('webapp:index')
+
+    elif request.method == "POST":
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('webapp:index')
+            else:
+                return redirect('webapp:index')
+
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+    return redirect('webapp:index')
+
 
    
